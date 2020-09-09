9e0578f03f209f7ed4d8a976c94335d02840980c
Andrevlc <andrevezi@gmail.com>
2020-09-09 17:18:35 -0300

    CC fixed

--- src/Services/foodAPI.js
+++ src/Services/foodAPI.js
@@ -8,41 +8,41 @@ export default function tokenAPI() {
 export const getFoodByName = (name) => {
   const URL = `${foodUrl}search.php?s=${name}`;
   return fetch(URL).then((response) =>
-    response.json().then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json)))
+    response.json().then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json))),
   );
 };
 
 export const getFoodByIngredients = (ingredient) => {
   const URL = `${foodUrl}filter.php?i=${ingredient}`;
   return fetch(URL).then((response) =>
-    response.json().then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json)))
+    response.json().then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json))),
   );
 };
 
 export const getFoodByLetter = (letter) => {
   const URL = `${foodUrl}search.php?f=${letter}`;
   return fetch(URL).then((response) =>
-    response.json().then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json)))
+    response.json().then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json))),
   );
 };
 
 export const getFoodCategories = () => {
   const URL = `${foodUrl}list.php?c=list`;
   return fetch(URL).then((response) =>
-    response.json().then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json)))
+    response.json().then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json))),
   );
 };
 
 export const getFoodIngredients = () => {
   const URL = `${foodUrl}list.php?i=list`;
   return fetch(URL).then((response) =>
-    response.json().then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json)))
+    response.json().then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json))),
   );
 };
 
 export const getRandomFood = () => {
   const URL = `${foodUrl}random.php`;
   return fetch(URL).then((response) =>
-    response.json().then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json)))
+    response.json().then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json))),
   );
 };
