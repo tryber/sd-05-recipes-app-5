ea15922483f85491309a95e7f91d27e6051cc147
luialbeto <araujo.script@gmail.com>
2020-09-09 11:34:53 -0300

    codeclimate componentes criados & App

--- src/components/Bebidas.js
+++ src/components/Bebidas.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Bebidas extends Component {
   render() {
     return (
       <div>
-        
+        Bebidas
       </div>
-    )
+    );
   }
 }
--- src/components/Comidas.js
+++ src/components/Comidas.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Comidas extends Component {
   render() {
     return (
       <div>
-        
+        Comidas
       </div>
-    )
+    );
   }
 }
