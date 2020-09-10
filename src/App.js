d7d1767061bd0a13b4095b23a46031b56ff175f0
Rafael Assad <rafaassadeng@gmail.com>
2020-09-10 15:52:51 -0300

    loooo

--- src/App.js
+++ src/App.js
@@ -3,21 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from './Pages/Login';
 import './App.css';
 
-
 function App() {
   return (
     <div>
       <BrowserRouter>
-      <Header />
         <Switch>
           <Route exact path="/" component={Login} />
         </Switch>
       </BrowserRouter>
-      <Login />
-      <Explorar />
-      <Comidas />
-      <Bebidas />
-      <Footer />
     </div>
   );
 }
