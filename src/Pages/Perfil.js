f94efc32bef2d4fe4d8df6dff2666775dba551f1
luialbeto <araujo.script@gmail.com>
2020-09-13 19:32:26 -0300

    implementando profile

---
+++ src/Pages/Perfil.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+export default class Perfil extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Footer />
+      </div>
+    )
+  }
+}
--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import thunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducers from '../reducers/index';
-
-const store = createStore(rootReducers, composeWithDevTools(applyMiddleware(thunk)));
-
-export default store;
